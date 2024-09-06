5e5ce44ee4a98f2e4aa9180e65dd4d56efefb231
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2024-09-06 10:33:07 +0200

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename='/spa-countries-vite'>
+    <BrowserRouter basename='/spa-countries-vite/'>
       <App />
     </BrowserRouter>
   </StrictMode>
